490a0b522e3f233fd81db9f6eb81dbc5583e8af9
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-05-24 16:32:47 +0900

    Initial commit

---
+++ src/main/java/com/example/restapi/NameController.java
@@ -0,0 +1,15 @@
+package com.example.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/names")
+    public List<String> getNames() {
+        return List.of("ebihara", "yamada");
+    }
+}
