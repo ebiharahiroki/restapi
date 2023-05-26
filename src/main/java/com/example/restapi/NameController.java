27ecac2f445cb0adab6c693cc5a8b71ca396a5d1
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-05-26 13:32:55 +0900

    登録処理を追加

--- src/main/java/com/example/restapi/NameController.java
+++ src/main/java/com/example/restapi/NameController.java
@@ -1,8 +1,13 @@
 package com.example.restapi;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -10,6 +15,16 @@ public class NameController {
 
     @GetMapping("/names")
     public List<String> getNames() {
-        return List.of("ebihara", "yamada", "suzuki");
+        return List.of("ebihara", "yamada", "suzuki", "tanaka");
+    }
+
+    @PostMapping("/names")
+    public ResponseEntity<String> createName(@RequestBody NameCreateForm nameCreateForm) {
+        // 登録処理は割愛
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080").path("/names/id")
+                .build()
+                .toUri();
+
+        return ResponseEntity.created(url).body("name successfully created");
     }
 }
---
+++ src/main/java/com/example/restapi/README.md
@@ -0,0 +1,7 @@
+curl 'http://localhost:8080/names' \
+-H 'Content-Type: application/json' \
+-d '{
+"name": "koyama"
+"age": 20
+"address": "Itabashi, Tokyo"
+}'
